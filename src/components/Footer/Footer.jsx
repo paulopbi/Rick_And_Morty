91870719e5058a8d0a97314905028de29f517988
Paulo Victor <paulpbi16@gmail.com>
2024-09-12 21:09:52 -0300

    feat: footer done

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p className="footer__text">
+        Desenvolvido por Paulo Victor, 2024 &copy;.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
