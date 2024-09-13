1bf742c9011127af8db6201417f0cc49c2bdf2f9
Paulo Victor <paulpbi16@gmail.com>
2024-09-12 21:09:19 -0300

    feat: react router dom initial routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar/NavBar";
+import Home from "./pages/Home/Home";
+import Footer from "./components/Footer/Footer";
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
